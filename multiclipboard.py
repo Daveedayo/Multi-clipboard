9170a34997af66e7778d29ce3867147cff0e1d94
Daveed <daveed925@gmail.com>
2022-08-26 22:57:10 +0100

    Added new features

--- multiclipboard.py
+++ multiclipboard.py
@@ -2,7 +2,26 @@ import sys
 import clipboard
 import json
 
+'''
 data = clipboard.paste()
 print(data)
 
 clipboard.copy("abc")
+'''
+'''
+print(sys.argv[1:])
+'''
+
+if len(sys.argv) == 2:
+    command = sys.argv[1]
+    
+    if command == "save":
+        print("save")
+    elif command == "load":
+        print("load")
+    elif command == "list":
+        print("list")
+    else:
+        print("Unknown command")
+else:
+    print("Please pass exactly one command")
