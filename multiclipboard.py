875ad6bd4a94263df5d58e9799f6047813252d8d
Daveed <daveed925@gmail.com>
2022-08-26 23:35:52 +0100

    Added new features

--- multiclipboard.py
+++ multiclipboard.py
@@ -12,7 +12,9 @@ clipboard.copy("abc")
 print(sys.argv[1:])
 '''
 
-def save_items(filepath, data):
+SAVED_DATA = "clipboard.json"
+
+def save_data(filepath, data):
     with open(filepath, "w") as f:
         json.dump(data, f)
 
@@ -20,16 +22,23 @@ def save_items(filepath, data):
 save_items("test.json", {"key": "value"})
 '''
 
-def load_items(filepath):
-    with open(filepath, "r") as f:
-        data = json.load(f)
-        return data
+def load_data(filepath):
+    try:
+        with open(filepath, "r") as f:
+            data = json.load(f)
+            return data
+    except:
+        return {}
 
 if len(sys.argv) == 2:
     command = sys.argv[1]
+    data = load_data(SAVED_DATA)
     
     if command == "save":
-        print("save")
+        key = input("Enter a key: ")
+        data[key] = clipboard.paste()
+        save_data(SAVED_DATA, data)
+        print("Data saved!")
     elif command == "load":
         print("load")
     elif command == "list":
